ca71ad7e8015a0085030133e0600b6facaedbfc1
Ayesha <ayesha.school@hotmail.com>
2021-09-05 15:25:06 -0400

    Update challenge_logic.js

--- challenge_logic.js
+++ challenge_logic.js
@@ -5,7 +5,7 @@
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	accessToken :API_KEY
+	accessToken :process.env.API_KEY
 });
 
 // We create the second tile layer that will be the background of our map.
