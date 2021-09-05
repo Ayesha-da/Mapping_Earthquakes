9fecd3b24f1c3eb50c90b2e9877612dfbe4fb63b
Ayesha <ayesha.school@hotmail.com>
2021-09-05 11:20:39 -0400

    Update challenge_logic.js

--- challenge_logic.js
+++ challenge_logic.js
@@ -15,6 +15,26 @@ app.listen(port, () => {
 app.use(express.static('index.html'));
 app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.get('/api', (request, response) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.json(data);
+  });
+});
+
+app.post('/api', (request, response) => {
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  response.json(data);
+});
 
 
   
