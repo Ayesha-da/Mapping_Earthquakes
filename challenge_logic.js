14a5a2316901d4ddd8998584b2b60e6936dcd427
Ayesha <ayesha.school@hotmail.com>
2021-09-05 15:27:14 -0400

    Update challenge_logic.js

--- challenge_logic.js
+++ challenge_logic.js
@@ -2,10 +2,10 @@
 
   
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={API_KEY}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	accessToken :process.env.API_KEY
+	
 });
 
 // We create the second tile layer that will be the background of our map.
